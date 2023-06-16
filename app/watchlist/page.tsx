66d63e8e284d944de1e827dfcf167432c6f1817d
qm402 <sauyeed@hotmail.co.uk>
2023-06-16 01:07:59 +0100

    added watch list page

---
+++ app/watchlist/page.tsx
@@ -0,0 +1,5 @@
+const WatchListPage = () => {
+  return <div>Watch List page</div>
+}
+
+export default WatchListPage
--- config/site.ts
+++ config/site.ts
@@ -8,5 +8,9 @@ export const siteConfig = {
       title: "Home",
       href: "/",
     },
+    {
+      title: "Watch list",
+      href: "/watchlist",
+    },
   ],
 }
